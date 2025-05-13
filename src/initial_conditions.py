be6e1a46817082d624d1d561a3eebfd0c3e5a3ef
xyzw12345 <wangjt2020@163.com>
2025-05-13 22:37:00 +0800

    minor update

--- src/initial_conditions.py
+++ src/initial_conditions.py
@@ -8,7 +8,7 @@ SUN_MASS = 1 # Mass of the central star
 DENSITY = 9.280e6 # Density of Earth
 
 def generate_test_disk(n_asteroids: int, max_particles: int, min_orbit_radius: float, max_orbit_radius: float,
-                       min_mass: float, max_mass: float) -> ParticleData:
+                       min_mass: float, max_mass: float, perturbation_scale: float) -> ParticleData:
     """
     Generates a central star and a disk of asteroids in the XY plane
     with roughly circular Keplerian velocities.
@@ -40,7 +40,6 @@ def generate_test_disk(n_asteroids: int, max_particles: int, min_orbit_radius: f
         vy = speed_circ * np.cos(theta)
         vz = 0
         # Add small random velocity component (e.g., 10% of circular speed)
-        perturbation_scale = 0.1
         vx += np.random.normal(0, perturbation_scale * speed_circ)
         vy += np.random.normal(0, perturbation_scale * speed_circ)
         vz += np.random.normal(0, perturbation_scale * speed_circ * 0.1) # Smaller z velocity perturbation
--- src/main.py
+++ src/main.py
@@ -6,6 +6,11 @@ def main():
     # --- Simulation Parameters ---
     NUM_ASTEROIDS = 1000       # Number of asteroids
     MAX_PARTICLES = NUM_ASTEROIDS + 1 # Capacity slightly larger than needed
+    MIN_ORBIT_RADIUS = 0.95
+    MAX_ORBIT_RADUIS = 1.05
+    MIN_MASS = 1e-6
+    MAX_MASS = 3e-6
+    PERTURBATION_SCALE = 0.3
     TIME_STEP = 0.001       # Simulation time step (e.g., in years/2pi if G=1, SunMass=1, Dist=AU)
     NUM_STEPS = 30000        # Period of simulation
     PLOT_INTERVAL = 400       # Period of Saving plot
@@ -17,7 +22,8 @@ def main():
     print(f"Plot interval: {PLOT_INTERVAL}")
 
     # 1. Generate Initial Conditions
-    particles = generate_test_disk(n_asteroids=NUM_ASTEROIDS, max_particles=MAX_PARTICLES)
+    particles = generate_test_disk(n_asteroids=NUM_ASTEROIDS, max_particles=MAX_PARTICLES, min_orbit_radius=MIN_ORBIT_RADIUS,
+                                   max_orbit_radius=MAX_ORBIT_RADUIS, min_mass=MIN_MASS, max_mass=MAX_MASS, perturbation_scale=PERTURBATION_SCALE)
 
     # 2. Create Simulation Instance
     sim = Simulation(particles) # Uses default G and epsilon from simulation.py
