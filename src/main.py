93ef2b442551c48e7beab3db74a1b2163d81973d
xyzw12345 <wangjt2020@163.com>
2025-05-15 17:00:59 +0800

    chore: minor update on visulization

--- src/main.py
+++ src/main.py
@@ -5,17 +5,17 @@ from cProfile import run
 
 def main():
     # --- Simulation Parameters ---
-    NUM_ASTEROIDS = 10000     # Number of asteroids
+    NUM_ASTEROIDS = 1000     # Number of asteroids
     MAX_PARTICLES = NUM_ASTEROIDS + 1 # Capacity slightly larger than needed
     MIN_ORBIT_RADIUS = 0.95
-    MAX_ORBIT_RADUIS = 1.05
+    MAX_ORBIT_RADIUS = 1.05
     MIN_MASS = 1e-5
     MAX_MASS = 3e-5
     PERTURBATION_SCALE = 0.1
     ETA_VALUE = 0.3
     TIME_STEP = 0.001       # Simulation time step in years/2pi
-    NUM_STEPS = 30        # Period of simulation
-    PLOT_INTERVAL = 1       # Period of Saving plot
+    NUM_STEPS = 300000        # Period of simulation
+    PLOT_INTERVAL = 1000       # Period of Saving plot
 
     print("--- N-Body Simulation Setup ---")
     print(f"Number of asteroids: {NUM_ASTEROIDS}")
@@ -24,11 +24,11 @@ def main():
     print(f"Plot interval: {PLOT_INTERVAL}")
 
     # 1. Generate Initial Conditions
-    # particles = generate_test_disk(n_asteroids=NUM_ASTEROIDS, max_particles=MAX_PARTICLES, min_orbit_radius=MIN_ORBIT_RADIUS,
-    #                                max_orbit_radius=MAX_ORBIT_RADUIS, min_mass=MIN_MASS, max_mass=MAX_MASS, perturbation_scale=PERTURBATION_SCALE)
-    particles = ParticleData(3)
-    particles.add_particle([1, 0, 0], [0, 0, 0], 1)
-    particles.add_particle([-1, 0, 0], [0, 0, 0], 1)
+    particles = generate_test_disk(n_asteroids=NUM_ASTEROIDS, max_particles=MAX_PARTICLES, min_orbit_radius=MIN_ORBIT_RADIUS,
+                                   max_orbit_radius=MAX_ORBIT_RADIUS, min_mass=MIN_MASS, max_mass=MAX_MASS, perturbation_scale=PERTURBATION_SCALE)
+    # particles = ParticleData(3)
+    # particles.add_particle([1, 0, 0], [0, 0, 0], 1)
+    # particles.add_particle([-1, 0, 0], [0, 0, 0], 1)
 
     # 2. Create Simulation Instance
     sim = Simulation(particles) # Uses default G and epsilon from simulation.py
